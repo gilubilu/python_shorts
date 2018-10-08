bbaf7096d163e7fc38303f53a3df851807f1b65a
gilubilu <gilbert.micallef@gmail.com>
2018-10-08 20:49:21 +0200

    Update testing/basics.py

--- testing/basics.py
+++ testing/basics.py
@@ -1,1 +1,12 @@
 print("Hello World")
+
+a = 8
+b = 0
+
+try:
+    c = a/b
+    print(c)
+except ConnectionError:
+    print("connection error")
+except ZeroDivisionError:
+    print("error when dividing by zero")
